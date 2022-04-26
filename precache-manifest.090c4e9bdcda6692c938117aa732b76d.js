ca985ecf6f2c6555e36b0f92ad92188257a8d955
gifnft <104457510+gifnft@users.noreply.github.com>
2022-04-27 02:20:21 +0800

    Add files via upload

---
+++ precache-manifest.090c4e9bdcda6692c938117aa732b76d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07d4ccbbccc3531d4edbf97ab6c9f720",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a21316773a1c848e9f9",
+    "url": "/static/css/2.4428d2e8.chunk.css"
+  },
+  {
+    "revision": "a82009e32eef25775e4e",
+    "url": "/static/css/main.8ffb9131.chunk.css"
+  },
+  {
+    "revision": "0a21316773a1c848e9f9",
+    "url": "/static/js/2.c066222c.chunk.js"
+  },
+  {
+    "revision": "a82009e32eef25775e4e",
+    "url": "/static/js/main.0855c500.chunk.js"
+  },
+  {
+    "revision": "09e1e2af67ef853ca650",
+    "url": "/static/js/runtime-main.cb1e6677.js"
+  }
+]);
